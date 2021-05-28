19c430644d45662b87aeb35c0aa5f89bf8ae46e1
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Pro.local>
2021-05-28 00:23:26 -0700

    about and contact routes added

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get("/", function(req, res) {
   res.render("home", {homeContent : homeStartingContent})
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", { aboutContent: aboutContent})
+});
+
+
+app.get("/contact", function(req, res) {
+  res.render("contact", { contactContent: contactContent})
+});
 
 
 
