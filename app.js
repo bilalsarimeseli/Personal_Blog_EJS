79cb228439b2b2836d32f35f723ca04010c5c32c
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Pro.local>
2021-06-06 22:05:24 -0700

    button console logging body text

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
 app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent })
 });
@@ -35,8 +36,8 @@ app.get("/compose", function (req, res) {
   res.render("compose")
 })
 
-app.post("/compose", function(req, res){
-  console.log(req.body.toBePosted);
+app.post("/compose", function (req, res) {
+  console.log(req.body.postedText);
   res.redirect("/");
 })
 
