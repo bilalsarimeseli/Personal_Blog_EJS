182db087ac796993a2482515b325bdfe77c903c5
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Pro.local>
2021-06-24 14:38:58 -0700

    lodash installed everything lowercase applied in user entry

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,9 +22,9 @@ app.use(express.static('public'));
 let posts = [];
 
 app.get("/posts/:parameter", function(req, res){
-  const userEntry = req.params.parameter;
+  const userEntry = _.lowerCase(req.params.parameter);
   for(var i = 0; i < posts.length; i ++){
-    if (posts[i].title === userEntry ){
+    if (_.lowerCase(posts[i].title) === userEntry ){
       console.log("Match Found");
     } 
   }
